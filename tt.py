1bacd9ca9f6bbfad33b6c86bd1e05f97de36b852
Ana Parra <93445926+anacsparra@users.noreply.github.com>
2024-03-28 09:27:42 -0300

    Add files via upload

---
+++ tt.py
@@ -0,0 +1,39 @@
+    
+    #opções para escolha
+     if opcao == 1:
+        #1. Número de palavras
+
+
+    elif opcao == 2:
+        #2. Número de palavras distintas
+
+    elif opcao == 3:
+        #3. Número de linhas
+
+    elif opcao == 4:
+        #4. Frequência das palavras
+
+    elif opcao == 5:
+        #5. Imprimir uma linha específica
+
+    elif opcao == 6:
+        #6. Buscar uma palavra e imprimir a linha em que aparece a primeira ocorrência
+
+    elif opcao == 7:
+        #7. Substituir todas as ocorrências de uma palavra por outra
+
+    elif opcao == 8:
+        #8. Abrir um outro livro
+
+    elif opcao == 9:
+        #9. Encerrar o programa
+
+    #elif opcao == 10: --> SE DER: 10. Gerar nuvem de palavras
+
+    else:
+        print("Opção inválida.")
+
+
+
+
+#frequência de palavras: https://programminghistorian.org/pt/licoes/contar-frequencias-palavras-python
